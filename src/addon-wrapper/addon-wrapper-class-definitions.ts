93fdde378a788f4c24e4aa934fd884df5d208103
Rostislav Provodenko <rostislav.provodenko@deepcrawl.com>
2020-10-02 16:02:08 +0300

    cleanup

--- src/addon-wrapper/addon-wrapper-class-definitions.ts
+++
@@ -1,8 +0,0 @@
-export declare class ConnectionWrapper {
-    execute(command: string): ResultWrapper;
-}
-  
-export declare class ResultWrapper {
-    public fetchRow(): any[];
-    public describe(): string[][];
-}
