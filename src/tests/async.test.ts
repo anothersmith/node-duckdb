2ccc08298ab749f64e66a64c9ec20e5092fd4935
Rostislav Provodenko <rostislav.provodenko@deepcrawl.com>
2020-10-28 13:46:50 +0300

    fix

--- src/tests/async.test.ts
+++ src/tests/async.test.ts
@@ -1,83 +1,51 @@
-/* eslint-disable sonarjs/no-duplicate-string */
 import { ConnectionWrapper } from "index";
+
+const query1 = "SELECT * FROM read_csv_auto('src/tests/test-fixtures/web_page.csv')";
+const query2 = "SELECT count(*) FROM read_csv_auto('src/tests/test-fixtures/web_page.csv')";
+const result1 = [
+  1,
+  "AAAAAAAABAAAAAAA",
+  873244800000,
+  null,
+  2450810,
+  2452620,
+  "Y",
+  98539,
+  "http://www.foo.com",
+  "welcome",
+  2531,
+  8,
+  3,
+  4,
+];
+const result2 = [60];
 
 describe("Async execute", () => {
   it("can do concurrent operations with same ConnectionWrapper", async () => {
     const cw = new ConnectionWrapper();
-    const p1 = cw.execute("SELECT * FROM read_csv_auto('src/tests/test-fixtures/web_page.csv')");
-
-    const p2 = cw.execute("SELECT count(*) FROM read_csv_auto('src/tests/test-fixtures/web_page.csv')");
+    const p1 = cw.execute(query1);
+    const p2 = cw.execute(query2);
     const [rw1, rw2] = await Promise.all([p1, p2]);
-    expect(rw1.fetchRow()).toMatchObject([
-      1,
-      "AAAAAAAABAAAAAAA",
-      873244800000,
-      null,
-      2450810,
-      2452620,
-      "Y",
-      98539,
-      "http://www.foo.com",
-      "welcome",
-      2531,
-      8,
-      3,
-      4,
-    ]);
-
-    expect(rw2.fetchRow()).toMatchObject([60]);
+    expect(rw1.fetchRow()).toMatchObject(result1);
+    expect(rw2.fetchRow()).toMatchObject(result2);
   });
 
   it("can do concurrent operations with different ConnectionWrapper", async () => {
     const cw1 = new ConnectionWrapper();
     const cw2 = new ConnectionWrapper();
-    const p1 = cw1.execute("SELECT * FROM read_csv_auto('src/tests/test-fixtures/web_page.csv')");
-
-    const p2 = cw2.execute("SELECT count(*) FROM read_csv_auto('src/tests/test-fixtures/web_page.csv')");
+    const p1 = cw1.execute(query1);
+    const p2 = cw2.execute(query2);
     const [rw1, rw2] = await Promise.all([p1, p2]);
-    expect(rw1.fetchRow()).toMatchObject([
-      1,
-      "AAAAAAAABAAAAAAA",
-      873244800000,
-      null,
-      2450810,
-      2452620,
-      "Y",
-      98539,
-      "http://www.foo.com",
-      "welcome",
-      2531,
-      8,
-      3,
-      4,
-    ]);
-
-    expect(rw2.fetchRow()).toMatchObject([60]);
+    expect(rw1.fetchRow()).toMatchObject(result1);
+    expect(rw2.fetchRow()).toMatchObject(result2);
   });
 
   it("can do a consequtive operations", async () => {
     const cw = new ConnectionWrapper();
-
-    const rw1 = await cw.execute("SELECT * FROM read_csv_auto('src/tests/test-fixtures/web_page.csv')");
-    expect(rw1.fetchRow()).toMatchObject([
-      1,
-      "AAAAAAAABAAAAAAA",
-      873244800000,
-      null,
-      2450810,
-      2452620,
-      "Y",
-      98539,
-      "http://www.foo.com",
-      "welcome",
-      2531,
-      8,
-      3,
-      4,
-    ]);
-
-    const rw2 = await cw.execute("SELECT count(*) FROM read_csv_auto('src/tests/test-fixtures/web_page.csv')");
-    expect(rw2.fetchRow()).toMatchObject([60]);
+    const rw1 = await cw.execute(query1);
+    expect(rw1.fetchRow()).toMatchObject(result1);
+    const rw2 = await cw.execute(query2);
+    expect(rw2.fetchRow()).toMatchObject(result2);
   });
 
   jest.setTimeout(60000 * 5);
@@ -86,8 +54,8 @@ describe("Async execute", () => {
     let didEventLoopBlock = false;
     const timer = setInterval(() => {
       const currentDate = new Date();
-      // if setInterval hasn't fired within last 200 ms then assume event loop has been blocked
-      if (new Date(lastDate.getTime() + 200) < currentDate) {
+      // if setInterval hasn't fired within last 800 ms then assume event loop has been blocked
+      if (new Date(lastDate.getTime() + 800) < currentDate) {
         didEventLoopBlock = true;
       }
       lastDate = currentDate;
