6ba8762f979f35430efa0461cb8b3b9589ce3554
Rostislav Provodenko <rostislav.provodenko@deepcrawl.com>
2020-11-04 12:57:42 +0300

    validation tests

--- src/tests/duckdb-config.test.ts
+++ src/tests/duckdb-config.test.ts
@@ -46,10 +46,18 @@ describe("DuckDB configuration", () => {
         await unlinkAsync(dbPath)
     });
 
+    it("does not allow to to specify invalid options object", async () => {
+        expect(() => new DuckDB(<any>{options: 1})).toThrow("Invalid options: must be an object");
+    });
+
     it("allows to specify checkpoint WAL size", async () => {
         const db = new DuckDB({options: {checkPointWALSize: 1 << 30}});
         expect(db.checkPointWALSize).toBe(1 << 30);
         db.close();
+    });
+
+    it("does not allow to specify invalid checkpoint WAL size", async () => {
+        expect(() => new DuckDB(<any>{options: {checkPointWALSize: "invalid"}})).toThrow("Invalid checkPointWALSize: must be a number");
     });
 
     it("allows to specify maximum memory", async () => {
@@ -58,10 +66,19 @@ describe("DuckDB configuration", () => {
         db.close();
     });
 
+    it("does not allow to specify invalid maximum memory", async () => {
+        expect(() => new DuckDB(<any>{options: {maximumMemory: "invalid"}})).toThrow("Invalid maximumMemory: must be a number");
+    });
+
+    // Note: looks like duckdb bug: sets useTemporaryDirectory to true (although at the same time removes temporaryDirectory value)
     it("allows to specify whether to use temp dir", async () => {
         const db = new DuckDB({options: {useTemporaryDirectory: false, temporaryDirectory: __dirname}});
         expect(db.temporaryDirectory).toBeFalsy();
         db.close();
+    });
+
+    it("does not allow to specify invalid useTemporaryDirectory", async () => {
+        expect(() => new DuckDB(<any>{options: {useTemporaryDirectory: "invalid"}})).toThrow("Invalid useTemporaryDirectory: must be a boolean");
     });
 
     it("allows to specify temp dir", async () => {
@@ -70,10 +87,18 @@ describe("DuckDB configuration", () => {
         db.close();
     });
 
+    it("does not allow to specify invalid temp dir", async () => {
+        expect(() => new DuckDB(<any>{options: {temporaryDirectory: true}})).toThrow("Invalid temporaryDirectory: must be a string");
+    });
+
     it("allows to specify collation", async () => {
         const db = new DuckDB({options: {collation: "binary"}});
         expect(db.collation).toBe("binary");
         db.close();
+    });
+
+    it("does not allow to specify invalid collation", async () => {
+        expect(() => new DuckDB(<any>{options: {collation: true}})).toThrow("Invalid collation: must be a string");
     });
 
     it("allows to specify default order type", async () => {
@@ -82,15 +107,27 @@ describe("DuckDB configuration", () => {
         db.close();
     });
 
+    it("does not allow to specify invalid default order type", async () => {
+        expect(() => new DuckDB(<any>{options: {defaultOrderType: 10}})).toThrow("Invalid defaultOrderType: must be of type OrderType enum");
+    });
+
     it("allows to specify default null order type", async () => {
         const db = new DuckDB({options: {defaultNullOrder: OrderByNullType.NullsLast}});
         expect(db.defaultNullOrder).toBe(OrderByNullType.NullsLast);
         db.close();
     });
 
+    it("does not allow to specify invalid default null order type", async () => {
+        expect(() => new DuckDB(<any>{options: {defaultNullOrder: 10}})).toThrow("Invalid defaultNullOrder: must be of type OrderType enum");
+    });
+
     it("allows to specify enable copy", async () => {
         const db = new DuckDB({options: {enableCopy: false}});
         expect(db.enableCopy).toBe(false);
         db.close();
+    });
+
+    it("does not allow to specify invalid enableCopy", async () => {
+        expect(() => new DuckDB(<any>{options: {enableCopy: 10}})).toThrow("Invalid enableCopy: must be a boolean");
     });
 })
