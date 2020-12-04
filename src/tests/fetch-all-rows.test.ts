eb54db34511de84ba05ef89fed611a6fcac792a3
Rostislav Provodenko <rostislav.provodenko@deepcrawl.com>
2020-12-04 18:46:03 +0300

    wip

--- src/tests/fetch-all-rows.test.ts
+++ src/tests/fetch-all-rows.test.ts
@@ -3,13 +3,13 @@ import { Connection, DuckDB } from "@addon";
 const jsonResult = {
   bigint_col: 0n,
   bool_col: true,
-  date_string_col: "03/01/09",
+  date_string_col: Buffer.from([48, 51, 47, 48, 49, 47, 48, 57]),
   double_col: 0,
   float_col: 0,
   id: 4,
   int_col: 0,
   smallint_col: 0,
-  string_col: "0",
+  string_col: Buffer.from([48]),
   timestamp_col: 1235865600000,
   tinyint_col: 0,
 };
--- src/tests/parquet.test.ts
+++ src/tests/parquet.test.ts
@@ -18,31 +18,52 @@ describe("executeIterator on parquet", () => {
 
   it("can do a count", async () => {
     const result = await connection.executeIterator(
-      "SELECT count(*) FROM parquet_scan('src/tests/test-fixtures/alltypes_plain.parquet')",
+      "SELECT count(*) FROM parquet_scan('src/tests/test-fixtures/data-types.parquet')",
       executeOptions,
     );
-    expect(result.fetchRow()).toMatchObject([8n]);
+    expect(result.fetchRow()).toMatchObject([5n]);
     expect(result.fetchRow()).toBe(null);
   });
 
   it("can do a select all", async () => {
     const result = await connection.executeIterator(
-      "SELECT * FROM parquet_scan('src/tests/test-fixtures/alltypes_plain.parquet')",
+      "SELECT * FROM parquet_scan('src/tests/test-fixtures/data-types.parquet')",
       executeOptions,
     );
-    expect(result.fetchRow()).toMatchObject([4, true, 0, 0, 0, 0n, 0, 0, "03/01/09", "0", 1235865600000]);
+    expect(result.fetchRow()).toMatchObject([null, null, null, null, null, null, null, null, null, null, null, null]);
     expect(result.fetchRow()).toMatchObject([
-      5,
-      false,
-      1,
-      1,
-      1,
-      10n,
-      1.100000023841858,
-      10.1,
-      "03/01/09",
-      "1",
-      1235865660000,
+      42,
+      43,
+      44,
+      45n,
+      4.599999904632568,
+      4.7,
+      480,
+      '49',
+      Buffer.from([53, 48]),
+      true,
+      1574799102501,
+      18271
     ]);
+  });
+
+  it("handles Object output format", async () => {
+    const result = await connection.executeIterator(
+      "SELECT * FROM parquet_scan('src/tests/test-fixtures/data-types.parquet')",
+    );
+    expect(result.fetchAllRows()[1]).toEqual({
+      byteval: 42,
+      shortval: 43,
+      integerval: 44,
+      longval: 45n,
+      floatval: 4.599999904632568,
+      doubleval: 4.7,
+      decimalval: 480,
+      stringval: '49',
+      binaryval: Buffer.from([53, 48]),
+      booleanval: true,
+      timestampval: 1574799102501,
+      dateval: 18271
+    });
   });
 });
