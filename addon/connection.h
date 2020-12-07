eb1e724beab01b4a5248154011a38749c527d766
Rostislav Provodenko <60982217+rostislavdeepcrawl@users.noreply.github.com>
2020-12-07 18:05:20 +0300

    cpp linting (#35)

--- addon/connection.h
+++ addon/connection.h
@@ -1,26 +1,26 @@
 #ifndef connection_H
 #define connection_H
 
-#include <napi.h>
 #include "duckdb.hpp"
-#include <vector>
 #include "result_iterator.h"
+#include <napi.h>
+#include <vector>
 
 namespace NodeDuckDB {
-  class Connection : public Napi::ObjectWrap<Connection> {
-    public:
-      static Napi::Object Init(Napi::Env env, Napi::Object exports);
-      Connection(const Napi::CallbackInfo& info);
+class Connection : public Napi::ObjectWrap<Connection> {
+public:
+  static Napi::Object Init(Napi::Env env, Napi::Object exports);
+  Connection(const Napi::CallbackInfo &info);
 
-    private:
-      static Napi::FunctionReference constructor;
-      Napi::Value Execute(const Napi::CallbackInfo& info);
-      Napi::Value Close(const Napi::CallbackInfo& info);
-			Napi::Value IsClosed(const Napi::CallbackInfo &info);
+private:
+  static Napi::FunctionReference constructor;
+  Napi::Value Execute(const Napi::CallbackInfo &info);
+  Napi::Value Close(const Napi::CallbackInfo &info);
+  Napi::Value IsClosed(const Napi::CallbackInfo &info);
 
-      shared_ptr<duckdb::DuckDB> database;
-      shared_ptr<duckdb::Connection> connection;
-      std::shared_ptr<std::vector<ResultIterator*>> results;
-  };
-}
+  shared_ptr<duckdb::DuckDB> database;
+  shared_ptr<duckdb::Connection> connection;
+  std::shared_ptr<std::vector<ResultIterator *>> results;
+};
+} // namespace NodeDuckDB
 #endif
--- addon/type-converters.h
+++ addon/type-converters.h
@@ -1,14 +1,17 @@
 #include "duckdb.h"
 #include "duckdb.hpp"
 
-namespace NodeDuckDB
-{
-    namespace TypeConverters
-    {
-        string convertString(const Napi::Env &env, const Napi::Object &options, const string propertyName);
-        int32_t convertNumber(const Napi::Env &env, const Napi::Object &options, const string propertyName);
-        bool convertBoolean(const Napi::Env &env, const Napi::Object &options, const string propertyName);
-        int32_t convertEnum(const Napi::Env &env, const Napi::Object &options, const string propertyName, const int min, const int max);
-        void setDBConfig(const Napi::Env &env, const Napi::Object &config, duckdb::DBConfig &nativeConfig);
-    }
-}
+namespace NodeDuckDB {
+namespace TypeConverters {
+string convertString(const Napi::Env &env, const Napi::Object &options,
+                     const string propertyName);
+int32_t convertNumber(const Napi::Env &env, const Napi::Object &options,
+                      const string propertyName);
+bool convertBoolean(const Napi::Env &env, const Napi::Object &options,
+                    const string propertyName);
+int32_t convertEnum(const Napi::Env &env, const Napi::Object &options,
+                    const string propertyName, const int min, const int max);
+void setDBConfig(const Napi::Env &env, const Napi::Object &config,
+                 duckdb::DBConfig &nativeConfig);
+} // namespace TypeConverters
+} // namespace NodeDuckDB
