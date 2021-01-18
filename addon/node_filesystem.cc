bed5c65edc8a2ca24d4f8e68c12a0186c6eb11f4
Rostislav Provodenko <rostislav.provodenko@deepcrawl.com>
2021-01-18 18:30:22 +0300

    sync working

--- addon/node_filesystem.cc
+++ addon/node_filesystem.cc
@@ -3,6 +3,7 @@
 #include <iostream>
 using namespace duckdb;
 using namespace std;
+using namespace Napi;
 namespace NodeDuckDB {
 
 NodeFileSystem::NodeFileSystem(Napi::ThreadSafeFunction &filesystem_callback)
@@ -17,8 +18,33 @@ NodeFileSystem::OpenFile(const char *path, uint8_t flags,
 
 void NodeFileSystem::Read(FileHandle &handle, void *buffer, int64_t nr_bytes,
                           idx_t location) {
+  std::condition_variable cv;
+  std::mutex mtx;
+  std::unique_lock<std::mutex> lk(mtx);
+  bool ready = false;
+  auto callback = [&](Napi::Env env, Function jsCallback, int *value) {
+    cout << "fdsafdsfdsfdsafds" << endl;
+    auto fn = Napi::Function::New(
+        env,
+        [&](const Napi::CallbackInfo &info) { 
+          cout << "in callback" << endl; 
+          // std::unique_lock<std::mutex> lk(mtx);
+          ready = true;
+          cv.notify_one();
+          },
+        "theFunction");
+    // Transform native data into JS data, passing it to the provided
+    // `jsCallback` -- the TSFN's JavaScript function.
+    jsCallback.Call({fn});
+  };
 
-  filesystem_callback.BlockingCall();
+  int *value = new int(clock());
+  filesystem_callback.BlockingCall(value, callback);
+  cout << "locking" << endl;
+  cout << "waiting" << endl;
+  while (!ready)
+    cv.wait(lk);
+  cout << "unlocked" << endl;
 
   return duckdb::FileSystem::Read(handle, buffer, nr_bytes, location);
 }
--- src/tests/node-filesystem.test.ts
+++ src/tests/node-filesystem.test.ts
@@ -1,13 +1,15 @@
 import { Connection, DuckDB } from "@addon";
 import { AccessMode } from "@addon-types";
+/* eslint-disable */
 
 describe("Node filesystem", () => {
   it("allows reading from file", async () => {
-    const db = new DuckDB({ options: { accessMode: AccessMode.ReadWrite, useDirectIO: false } }, () => {
-      // eslint-disable-next-line no-console
+    const db = new DuckDB({ options: { accessMode: AccessMode.ReadWrite, useDirectIO: false } }, <any> ((a: any) => {
       console.log("In nodejs fs");
-    });
-    db.accessMode;
+      console.log(a);
+      a();
+
+    }));
     const connection1 = new Connection(db);
     const result = await connection1.executeIterator(
       "SELECT count(*) FROM parquet_scan('src/tests/test-fixtures/alltypes_plain.parquet')",
